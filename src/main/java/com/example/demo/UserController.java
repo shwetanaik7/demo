8da90eb6d31ce4131e66c707f7f0eecf20bbc976
shwetanaik7 <shwetabootcamp@gmail.com>
2022-01-19 02:03:32 -0500

    added database successfully

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,9 +8,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
+import com.example.demo.service.IUserService;
 
 @RestController
 public class UserController {
+	@Autowired
+	
+	IUserService userService;
+	
 	@GetMapping("/user")
 	String getUser() {
 		System.out.println("testing");
@@ -19,6 +25,7 @@ public class UserController {
 	void createUser(@RequestBody User user)
 	{
 		System.out.println(user.getName());
+		userService.saveUser(user);
 	}
 	@DeleteMapping("/user")
 	void deleteUser()
