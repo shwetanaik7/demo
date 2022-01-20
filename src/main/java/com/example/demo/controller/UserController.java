26b523635d455375d0a7c18c7cc3504286e06e3b
shwetanaik7 <shwetabootcamp@gmail.com>
2022-01-20 04:48:49 -0500

    get mapping done

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,15 +20,20 @@ public class UserController {
 	IUserService userService;
 	
 	@GetMapping("/user")
-	String getUser() {
-		System.out.println("testing");
-		return "testing";
+		Iterable<User> getUsers() {
+			return userService.getUsers();
+		}
+		
+	@GetMapping("/user/{id}")
+	Optional<User> getUsers(@PathVariable("id") Integer id) {
+		return userService.getUser(id);
 	}
+	
 	@PostMapping("/user")
 	void createUser(@RequestBody User user)
 	{
 		System.out.println(user.getName());
-		System.out.println(user.getAge());
+		
 		userService.saveUser(user);
 	}
 	@DeleteMapping("/user")
--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -5,13 +5,19 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.UserRepository;
 import com.example.demo.entity.User;
-
+import java.util.Optional;
 @Service
 public class IUserService {
 	@Autowired  //DI
 	UserRepository userRepository;
 	public void saveUser(User user) {
 		userRepository.save(user);
+	}
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+	public Optional<User> getUser(Integer id) {
+		return userRepository.findById(id);
 	}
 
 }
