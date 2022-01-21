8aaf25ca59816cc926bca5dd28968c6b2d8b92b4
shwetanaik7 <shwetabootcamp@gmail.com>
2022-01-21 04:25:02 -0500

    added Transactional annotation

--- src/main/java/com/example/demo/service/IUserService.java
+++ src/main/java/com/example/demo/service/IUserService.java
@@ -6,12 +6,16 @@ import org.springframework.stereotype.Service;
 import com.example.demo.UserRepository;
 import com.example.demo.entity.User;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 @Service
 public class IUserService {
 	@Autowired  //DI
 	UserRepository userRepository;
+	@Transactional
 	public void saveUser(User user) {
 		userRepository.save(user);
+		throw new IllegalArgumentException();
 	}
 	public Iterable<User> getUsers() {
 		return userRepository.findAll();
